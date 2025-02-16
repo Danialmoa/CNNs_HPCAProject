c1f8b530f28fa0d1877949985d8e5c9f9bbdb539
danial.moafi <danial.moafi@snapp.cab>
2025-02-16 19:54:36 +0100

    fix bugs

--- cuda/main.cu
+++ cuda/main.cu
@@ -74,7 +74,7 @@ int main() {
         std::cout << "Number of SMs: " << prop.multiProcessorCount << std::endl;
 
         // Training hyperparameters
-        const std::vector<int> batch_sizes = {32};
+        const std::vector<int> batch_sizes = {512};
         const int num_epochs = 1;
         const float learning_rate = 0.005f;
         
@@ -151,7 +151,7 @@ int main() {
                     // Compute loss and accuracy
                     float batch_loss = fc.compute_loss(d_batch_labels, batch_size);
                     float batch_accuracy = calculate_accuracy(d_fc_output, d_batch_labels, batch_size);
-                    std::cout << "Batch " << batch + 1 << "/" << num_batches << " - Loss: " << batch_loss << " - Accuracy: " << batch_accuracy * 100 << "%" << std::endl;
+
                     epoch_loss += batch_loss;
                     epoch_accuracy += batch_accuracy;
                     
