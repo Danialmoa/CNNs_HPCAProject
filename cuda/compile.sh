9a1594ddeb7fbd8be217a781e315dae4671d0aca
danial.moafi <danial.moafi@snapp.cab>
2025-02-17 11:36:05 +0100

    fix bugs

--- cuda/compile.sh
+++ cuda/compile.sh
@@ -5,6 +5,10 @@ nvcc -o cpp_cuda \
     conv_block.cu \
     fully_connected.cu \
     adam_optimizer.cu \
+    kernels/conv_kernels.cu \
+    kernels/activation_kernels.cu \
+    kernels/pooling_kernels.cu \
+    kernels/batchnorm_kernels.cu \
     -I./include \
     -arch=sm_52 \
     -std=c++14 \
